e51e3ef1f761cb713a00b16aab287bb932a660dd
connellr023 <connell.reffo@ucalgary.ca>
2024-04-20 00:07:08 -0600

    Got the basics

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # clipsort
-> A CLI tool that utilizes OpenAI's CLIP model to recognize content in images and sort them.
+> A CLI tool that utilizes the ResNet18 AI model to recognize content in images and sort them.
 
 ![Rust](https://img.shields.io/badge/rust-%23000000.svg?style=for-the-badge&logo=rust&logoColor=white)
 ![PyTorch](https://img.shields.io/badge/PyTorch-%23EE4C2C.svg?style=for-the-badge&logo=PyTorch&logoColor=white)
+
+<br />
+
+#### Development Environment
+The pretrained model used for this project can be found <a href="https://github.com/LaurentMazare/tch-rs/releases/download/mw/resnet18.ot">here</a>.
+
+Setting up the development environment can be done by following the **README** from the **tch-rs** repository <a href="https://github.com/LaurentMazare/tch-rs">here</a>
+
+When installing *libtorch*, ensure that the version that supports **CUDA** is used.
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,44 @@
-fn main()
+use tch::{Device, Kind};
+use tch::nn::VarStore;
+use tch::vision::{
+	imagenet,
+	resnet
+};
+
+const IMAGE_PATH: &str = "/home/connell/Programming/model-stuff/images/barbet.jpg";
+const PRETRAINED_MODEL_PATH: &str = "/home/connell/Programming/model-stuff/resnet18.ot";
+
+fn main() -> Result<(), Box<dyn std::error::Error>>
 {
-    println!("Hello, world!");
+	// let descriptions = vec![
+	// 	"man",
+	// 	"dog",
+	// 	"bodybuilder",
+	// 	"bug",
+	// 	"ocean",
+	// 	"weird"
+	// ];
+
+	let device = Device::cuda_if_available();
+	let mut vs = VarStore::new(device);
+	print!("Device selected: {:?}\n", device);
+
+	let image = imagenet::load_image(IMAGE_PATH)?.to_device(vs.device());
+	let model = resnet::resnet18(&vs.root(), imagenet::CLASS_COUNT);
+
+	vs.load(PRETRAINED_MODEL_PATH)?;
+
+	let output = image
+		.unsqueeze(0)
+		.apply_t(&model, false)
+		.softmax(-1, Kind::Float);
+
+	println!("Top tensor embedding the image:\n{}", output);
+	
+	for (probability, class) in imagenet::top(&output, 5).iter()
+	{
+		println!("{:50} {:5.2}%", class, probability * 100.0);
+	}
+
+	Ok(())
 }
