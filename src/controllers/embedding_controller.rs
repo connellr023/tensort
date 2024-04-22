2a94393b2d481845f965710c84bb6f029f3dfce9
connellr023 <connell.reffo@ucalgary.ca>
2024-04-21 20:57:17 -0600

    Similarity threshold function

--- src/controllers/embedding_controller.rs
+++ src/controllers/embedding_controller.rs
@@ -24,19 +24,54 @@ fn cosine_similarity(t1: &Tensor, t2: &Tensor) -> f64
     dot_product.double_value(&[]) / (norm1.double_value(&[]) * norm2.double_value(&[]))
 }
 
-pub fn calc_pairwise_cosine_similarities(embeddings: &Vec<TensorPathTuple>) -> Vec<f64>
+pub fn calc_pairwise_cosine_similarities(embeddings: &[&Tensor]) -> Vec<f64>
 {
     let embedding_count = embeddings.len();
     let mut similarities = Vec::with_capacity(embedding_count * embedding_count);
 
     for i in 0..embedding_count {
         for j in 0..embedding_count {
-            let similarity = cosine_similarity(&embeddings[i].0, &embeddings[j].0);
+            let similarity = cosine_similarity(embeddings[i], embeddings[j]);
             similarities.push(similarity);
         }
     }
 
     similarities
+}
+
+pub fn calc_similarity_threshold(similarities: &[f64], class_count: usize) -> f64
+{
+    let similarities_per_class = similarities.len() / class_count;
+    let mut max_within_clusters = Vec::with_capacity(class_count);
+
+    // Calculate maxmimum similarity within each cluster
+    for i in 0..class_count {
+        let start = i * similarities_per_class;
+        let end = start + similarities_per_class;
+        let max = similarities[start..end]
+            .iter()
+            .cloned()
+            .fold(
+                std::f64::NEG_INFINITY,
+                |max, similarity| { max.max(similarity) }
+        );
+
+        max_within_clusters.push(max);
+    }
+
+    // Calculate minimum similarity between clusters
+    let min_between_clusters = max_within_clusters
+        .iter()
+        .fold(
+            std::f64::INFINITY,
+            |min, &max_similarity| { min.min(max_similarity) }
+    );
+
+    let sum = max_within_clusters
+        .iter()
+        .sum::<f64>();
+
+    ((sum / class_count as f64) + min_between_clusters) / 2.0
 }
 
 pub fn gen_image_embeddings(dir: &PathBuf, model: &ResNet18Model) -> Result<Vec<TensorPathTuple>>
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ mod errors;
 
 use std::error::Error;
 use std::env::args;
+use tch::Tensor;
+
 use crate::models::arguments_model::ArgumentsModel;
 use crate::models::resnet18_model::ResNet18Model;
 use crate::controllers::embedding_controller;
@@ -30,12 +32,20 @@ fn main() -> Result<(), Box<dyn Error>>
 		println!("path: {}\n", embedding.1.to_str().unwrap());
 	}
 
-	let similarities = embedding_controller::calc_pairwise_cosine_similarities(&embeddings);
+	let similarities = embedding_controller::calc_pairwise_cosine_similarities(
+		embeddings
+			.iter()
+			.map(|tuple| { &tuple.0 })
+			.collect::<Vec<&Tensor>>()
+			.as_slice()
+	);
 
 	for similarity in similarities.iter()
 	{
 		println!("similarity: {}\n", similarity);
 	}
+
+	println!("threshold: {}\n", embedding_controller::calc_similarity_threshold(similarities.as_slice(), 6));
 
 	// let embedding = model_handler.gen_embedding(IMAGE_PATH, |tensor| {
 	// 	tensor.softmax(-1, Kind::Float)
